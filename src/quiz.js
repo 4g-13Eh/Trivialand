eb5cf6756366861ee86606ea955e243a1dd6abb8
Shahin Amon <amons@bzz.ch>
2023-08-12 11:05:41 +0200

    worked on fetch method

--- src/quiz.js
+++ src/quiz.js
@@ -4,17 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('settingsForm').addEventListener("submit", (e) => {
         e.preventDefault();
 
+        console.log("Submit event triggered");
+
         const amount = document.getElementById('number').value;
         const category = document.getElementById('category').value;
         const difficulty = document.getElementById('difficulty').value;
         const type = document.getElementById('type').value;
 
-        const url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`;
-
-        function generateQuiz(){
-            fetch(url, {
-                method: 'GET'
-            })
+        let url = `https://opentdb.com/api.php?amount=${amount}&`
+        if (category !== 'any'){
+            url += `&category=${category}`
         }
+        if (difficulty !== 'any'){
+            url += `&difficulty=${difficulty}`
+        }
+        if (type !== 'any'){
+            url += `&type=${type}`
+        }
+
+        console.log("Final URL:", url);
+
+        console.log("Fetching data...");
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+
     });
+
 });
